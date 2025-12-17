590df656840b342cd4b9a9968707a106deeb7bab
Dima Panu <149871287+dishok1@users.noreply.github.com>
2025-12-17 12:37:52 +0200

    Add new homework_7.js
    add index.html

---
+++ homework_7.js
@@ -0,0 +1,15 @@
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+// let element = document.querySelector("[data-widget-name]");
+
+// alert(element.dataset.widgetName);
+// // чи
+// console.log(element.getAttribute("data-widget-name"));
